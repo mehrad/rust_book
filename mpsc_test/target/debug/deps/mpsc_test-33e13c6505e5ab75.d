056b81446aa2ac08bc8ec93156a9ed83ed517669
Mahrad <hossein.ataeefard@gmail.com>
2024-03-29 06:57:57 +0000

    Delete mpsc_test/target directory

--- mpsc_test/target/debug/deps/mpsc_test-33e13c6505e5ab75.d
+++
@@ -1,5 +0,0 @@
-/Users/m3hrad/Projects/rust_test/mpsc_test/target/debug/deps/mpsc_test-33e13c6505e5ab75: src/main.rs
-
-/Users/m3hrad/Projects/rust_test/mpsc_test/target/debug/deps/mpsc_test-33e13c6505e5ab75.d: src/main.rs
-
-src/main.rs:
--- mpsc_test/target/debug/mpsc_test.d
+++
@@ -1,1 +0,0 @@
-/Users/m3hrad/Projects/rust_test/mpsc_test/target/debug/mpsc_test: /Users/m3hrad/Projects/rust_test/mpsc_test/src/main.rs
