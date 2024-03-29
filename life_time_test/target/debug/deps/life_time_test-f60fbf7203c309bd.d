05261f37facd1bc526032802a83c14705f593dd7
Mahrad <hossein.ataeefard@gmail.com>
2024-03-29 06:57:34 +0000

    Delete life_time_test/target directory

--- life_time_test/target/debug/deps/life_time_test-f60fbf7203c309bd.d
+++
@@ -1,5 +0,0 @@
-/Users/m3hrad/Projects/rust_test/life_time_test/target/debug/deps/liblife_time_test-f60fbf7203c309bd.rmeta: src/main.rs
-
-/Users/m3hrad/Projects/rust_test/life_time_test/target/debug/deps/life_time_test-f60fbf7203c309bd.d: src/main.rs
-
-src/main.rs:
--- life_time_test/target/debug/life_time_test.d
+++
@@ -1,1 +0,0 @@
-/Users/m3hrad/Projects/rust_test/life_time_test/target/debug/life_time_test: /Users/m3hrad/Projects/rust_test/life_time_test/src/main.rs
